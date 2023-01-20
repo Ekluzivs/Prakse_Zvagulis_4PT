f9665ebfd7b6fec8e85ba43111888283c21d69b7
Ekluzivs <78723478+Ekluzivs@users.noreply.github.com>
2023-01-20 19:48:02 +0200

    Update uzd3.sh
    komentāri

--- bash uzdevumi/uzd3.sh
+++ bash uzdevumi/uzd3.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
+#izveido teksta failus
 touch vis_ip_adr.txt
 touch uniq_ip_adr.txt
 touch LV_IP.txt
 touch kontakti.txt
-grep -o -m 5000 -E "\b[^/ . : v]([0-9]{1,3}\.){3}[0-9]{1,3}\b" access.log > vis_ip_adr.txt
+#atrod visas IP adreses izmantojot EGREP, notiek pārbaude vai IP adresēm sastāv nevajadzīgi simboli pirms IP adreses, ja sastāv, netiek atlasīts
+#ja nesastāv (/ . : v) tad tiek izdrukāts teksta failā
+grep -o -E "\b[^/ . : v]([0-9]{1,3}\.){3}[0-9]{1,3}\b" access.log > vis_ip_adr.txt
+#atlasa unikālās IP adreses no visām ip adresēm
 sort -u vis_ip_adr.txt > uniq_ip_adr.txt
+#iztīra teksta failu
 > LV_IP.txt
 > kontakti.txt
+#notiek IP adrešu valsts meklēšana, ja ir Latvija, tad tiks izprintēts IP adrese kas ir no Latvijas 
 echo "notiek IP meklesana"
 for ip in `cat uniq_ip_adr.txt`
 do
 echo "$ip" >> LV_IP.txt
-whois $ip | grep country | grep CN >> LV_IP.txt
+whois $ip | grep country | grep LV >> LV_IP.txt
 done
+# Notiek Epastu meklēšana, izmantojot līdzīgu metodi, atrod IP, atrod kur ir abuse līnijā, tad atrod E pasta adresi no tās līnijas, kas tiks izprintēts
+# Tiek izprintēti ja satur vairākas IP adreses
 echo "notiek epastu meklesana un drukasana"
 for daudz in `cat uniq_ip_adr.txt`
 do
