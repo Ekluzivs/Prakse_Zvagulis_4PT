3dac81be3056169939da1951664aab1f498c252e
Ekluzivs <78723478+Ekluzivs@users.noreply.github.com>
2023-01-23 20:45:05 +0200

    Update uzd3.sh

--- bash uzdevumi/uzd3.sh
+++ bash uzdevumi/uzd3.sh
@@ -4,9 +4,9 @@ touch vis_ip_adr.txt
 touch uniq_ip_adr.txt
 touch LV_IP.txt
 touch kontakti.txt
-#atrod visas IP adreses izmantojot EGREP, notiek pārbaude vai IP adresēm sastāv nevajadzīgi simboli pirms IP adreses, ja sastāv, netiek atlasīts
-#ja nesastāv (/ . : v) tad tiek izdrukāts teksta failā
-grep -o -E "\b[^/ . : v]([0-9]{1,3}\.){3}[0-9]{1,3}\b" access.log > vis_ip_adr.txt
+#atrod visas IP adreses izmantojot egrep (grep -E)
+#tiek izvilkta IPv4, tiek pārbaudīta katra oktate paternā
+grep -o -E "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" access.log > vis_ip_adr.txt
 #atlasa unikālās IP adreses no visām ip adresēm
 sort -u vis_ip_adr.txt > uniq_ip_adr.txt
 #iztīra teksta failu
@@ -17,7 +17,7 @@ echo "notiek IP meklesana"
 for ip in `cat uniq_ip_adr.txt`
 do
 echo "$ip" >> LV_IP.txt
-whois $ip | grep country | grep LV >> LV_IP.txt
+whois $ip | grep -o LV >> LV_IP.txt
 done
 # Notiek Epastu meklēšana, izmantojot līdzīgu metodi, atrod IP, atrod kur ir abuse līnijā, tad atrod E pasta adresi no tās līnijas, kas tiks izprintēts
 # Tiek izprintēti ja satur vairākas IP adreses
