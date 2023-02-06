4d3a75178764cf6d529bcb4ff20abcc687a8bbd1
Ekluzivs <78723478+Ekluzivs@users.noreply.github.com>
2023-02-06 15:23:45 +0200

    Update manafunkc.py

--- web/Flask/manafunkc.py
+++ web/Flask/manafunkc.py
@@ -1,20 +1,27 @@
 from flask import Flask
-from cau.iplookup import iplookup
-funkc=Flask(__name__)
-funkc=register_blueprint(iplookup)
-
-@funkc.route('/ip-lookup')
-def lookup('IP'):
-        l=IP.split=('.')
+#importē subprocess, jo lookup finckijai to vajadzēs
+import subprocess as su
+def check(IP):
+        #sadala IP adresi, ņemot punktus nost
+        l=IP.split(".")
+        #pārbauda vai katra oktate nav garāka par 3 simboliem, līdz ko ir vairāk, return False
         if len(l)!=4:
                 print ("Not Valid IP Address")
                 return False
+        #pārbauda vai cipars ir cipars
         for x in l:
                 if not x.isdigit():
                         print ("Not Valid IP Address")
                         return False
                 i=int(x)
+                #notiek pārbaude vai ir starp 0-255
                 if i < 0 or i > 255:
                         print ("Not Valid IP Address")
                         return False
+        #ja viss sakrīt no pārbaudēm, va ir IPv4, tad returno True
         return True
+def lookup(IP):
+        #izmantojot subprocess Popen, var dabūt IP adreses informāciju
+        #stdout=su.PIPE ievada to 'failā', kurā pēc tam tas tiek returnots lasot un dekodējot to uz utf8
+        wh=su.Popen(['whois','-h','whois.cymru.com',IP], stdout=su.PIPE)
+        return wh.stdout.read().decode('utf-8') 
