e8d84aba9c7c574d3a754e8cd64f460c5e6fd530
Ekluzivs <78723478+Ekluzivs@users.noreply.github.com>
2023-02-06 15:18:28 +0200

    Update cau.py

--- web/Flask/cau.py
+++ web/Flask/cau.py
@@ -1,12 +1,34 @@
-from flask import flash, render_template, Flask, request, redirect, Blueprint
-
+from flask import flash, render_template, Flask, request
+#HTML faili atrodas /templates failā
 app=Flask(__name__, template_folder='templates')
-app=Blueprint('iplookup', __name__)
 
+# index lapa, nospiežot, url aizvedīs uz /ip-lookup
 @app.route('/')
 def index():
         return render_template('index.html')
 
-@app.route('/ip-lookup', methods=['GET', 'POST'])
+@app.route('/ip-lookup', methods=["GET", "POST"])
+def ipinsert():
+        if request.method == "POST":
+                #AiPe ir no HTML ievades lauka, IP ir variable kas ņem no AiPe ievadītā
+                #kurā tad IP tiek iemests ipchecker funkcijā kurā citā python failā
+                #notiek vai IP adrese ir IPv4
+                IP=request.form['AiPe']
+                ch=ipchecker(IP)
+                #ja nav, tad error kļūdu izmet
+                if ch == False:
+                        error = 'Invalid IP address'
+                else:
+                        #ja ir IPv4 tad notiek IPlookup funkcija, kurā tad rezultātu izmet jaunā lapā, ar atpakaļ pogu
+                        look=iplookup(IP)
+                        return render_template('lookup.html', look=look)
+        return render_template('ip-lookup.html')
+def ipchecker(IP):
+        #importē IPv4 pārbaudes funkciju
+        from manafunkc import check
+        return check(IP)
+
 def iplookup(IP):
-        return IP==request.form['IP']
+        #importē lookup funkciju
+        from manafunkc import lookup
+        return lookup(IP)
