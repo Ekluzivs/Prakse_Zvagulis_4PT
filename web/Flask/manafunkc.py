6496dff065031afebcd917770911fab5c9d04f9d
Ekluzivs <78723478+Ekluzivs@users.noreply.github.com>
2023-02-10 17:57:35 +0200

    Update manafunkc.py

--- web/Flask/manafunkc.py
+++ web/Flask/manafunkc.py
@@ -3,29 +3,27 @@ from flask import Flask
 import subprocess as su
 def check(IP):
         #sadala IP adresi, ņemot punktus nost
-                       # pagaidām šis kods neiet, tika mēģināts 5 šādi līdzīgi varianti
-                       # kā arī kopumā divi dažādi varianti, bet testējot tie printēja error
-                       # ja ievada tukšu lauku vai nepārbaudīja
-        if "," in IP:
-                IP.split(',')
-        else:
-                IP=IP.strip()
-        for b in IP:
+        IP=IP.split(", ")
+                for b in IP:
         #pārbauda vai katra oktate nav garāka par 3 simboliem, līdz ko ir vairāk, return False
-        if len(l)!=4:
-                return False
+                if len(l)!=4:
+                        return False
         #pārbauda vai cipars ir cipars
-        for x in l:
-                if not x.isdigit():
-                        return False
-                i=int(x)
+                for x in l:
+                        if not x.isdigit():
+                                return False
+                        i=int(x)
                 #notiek pārbaude vai ir starp 0-255
-                if i < 0 or i > 255:
-                        return False
+                        if i < 0 or i > 255:
+                                return False
         #ja viss sakrīt no pārbaudēm, va ir IPv4, tad returno True
-        return True
+                return True
 def lookup(IP):
+        for IR in IP
         #izmantojot subprocess Popen, var dabūt IP adreses informāciju
         #stdout=su.PIPE ievada to 'failā', kurā pēc tam tas tiek returnots lasot un dekodējot to uz utf8
-        wh=su.Popen(['whois','-h','whois.cymru.com',IP], stdout=su.PIPE)
+        wh=su.Popen(['whois','-h','whois.cymru.com',IR], stdout=su.PIPE)
         return wh.stdout.read().decode('utf-8') 
+
+
+# Lielākā problēma tagad ir lai atkārto uz visām IP adresēm uz cikla, printē tikai vienu ciparu no visa IP adreses
