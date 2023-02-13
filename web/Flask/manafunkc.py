8fe45b4d982affec1e2a430f1af8f48b801a5ff4
Ekluzivs <78723478+Ekluzivs@users.noreply.github.com>
2023-02-13 22:13:50 +0200

    Update manafunkc.py

--- web/Flask/manafunkc.py
+++ web/Flask/manafunkc.py
@@ -4,7 +4,8 @@ import subprocess as su
 def check(IP):
         #sadala IP adresi, ņemot punktus nost
         IP=IP.split(", ")
-                for b in IP:
+        for K in IP:
+                l=K.split(".")
         #pārbauda vai katra oktate nav garāka par 3 simboliem, līdz ko ir vairāk, return False
                 if len(l)!=4:
                         return False
@@ -17,13 +18,15 @@ def check(IP):
                         if i < 0 or i > 255:
                                 return False
         #ja viss sakrīt no pārbaudēm, va ir IPv4, tad returno True
-                return True
+        return True
 def lookup(IP):
         for IR in IP
         #izmantojot subprocess Popen, var dabūt IP adreses informāciju
         #stdout=su.PIPE ievada to 'failā', kurā pēc tam tas tiek returnots lasot un dekodējot to uz utf8
-        wh=su.Popen(['whois','-h','whois.cymru.com',IR], stdout=su.PIPE)
-        return wh.stdout.read().decode('utf-8') 
+                cm=(['whois','-h','whois.cymru.com',*IR.split()] 
+                wh=su.Popen(cm, stdout=su.PIPE)
+                out=wh.stdout.read().decode('utf-8')
+                return out.replace("\n","")
 
 
 # Lielākā problēma tagad ir lai atkārto uz visām IP adresēm uz cikla, printē tikai vienu ciparu no visa IP adreses
