72751a273e320bec3b9a97105efbf3939c46bb14
Ekluzivs <marks.zvagul@gmail.com>
2023-04-26 15:04:43 +0300

    added comments, updated the code

--- web/Flask-Mysql-Test/controller.py
+++ web/Flask-Mysql-Test/controller.py
@@ -1,29 +1,32 @@
 from flask import flash, render_template, Flask, request
-import manafunkc
+import functions
 import mysql.connector
+#Assign the templates folder
 app=Flask(__name__, template_folder='templates')
 conn=None
+# Assnigning INT to variables for inserting into table value
 t1=200
 t2=21525
 t3=0
 t4=2
-# Error issue, for some reason tests is not working, currently trying to fix it
 #Further note, the issue of why the database was not working, because the database wasn't created, apparently the database has to created before launching the docker-compose command
 #to create database: docker exec -it (container-id) bash
 #mysql -u root -p (after which you enter the password that is on the python file)
 #CREATE DATABASE (database name on python file, i.e. testingtesting123);
-#exit; and then run the docker-compose command
+#exit; and then run the docker-compose up command
 
 class Datubaze:
-	#Connect with the mysql image and then once connected, will use the database testingtesting123
-	def __init__(self, user='root', password='Parole1', database='testingtesting123', host='db'):
+	#This function is used to connect with the database if called by one of the other functions
+	def __init__(self, user='root', password='Parole1', database='mydb', host='db'):
 		self.connection=mysql.connector.connect(user=user, password=password, database=database, host=host)
 		self.cursor=self.connection.cursor()
-	#Drops, creates and fills out the tables
+	#this function drops, creates and fills out the tables
 	def tabuluievade(self):
-		#Since nolikt table has a foreign key, it has to be dropped first, then krasas, otherwise error will occur
+#The table that has foreign keys has to be dropped first, in this case tests, becuse nolikt and krasas are foreign keys, then nolikt because krasas is a foreign key, then krasas
+		self.cursor.execute('DROP TABLE IF EXISTS tests')
 		self.cursor.execute('DROP TABLE IF EXISTS nolikt')
 		self.cursor.execute('DROP TABLE IF EXISTS krasas')
+
 		self.cursor.execute('CREATE TABLE krasas (id_test INT PRIMARY KEY AUTO_INCREMENT, krasa VARCHAR(20) NOT NULL, prieksmets VARCHAR(30) NOT NULL)')
 		self.cursor.execute('INSERT INTO krasas(krasa, prieksmets) VALUES("galds","bruns"), ("melns","telefons"), ("melns", "meness"), ("balts","muspapirs"), ("zals","zimulis")')
 		self.cursor.execute('CREATE TABLE nolikt(id_test INT NOT NULL, id_nolikt INT PRIMARY KEY AUTO_INCREMENT, cena Double(5,2) NOT NULL, noliktava VARCHAR(25) NOT NULL, FOREIGN KEY (id_test) REFERENCES krasas(id_test))')
@@ -39,19 +42,8 @@ class Datubaze:
 		self.cursor.execute('CREATE TABLE tests(id_test INT, id_nolikt INT, testvalue INT, FOREIGN KEY (id_test) REFERENCES krasas(id_test), FOREIGN KEY (id_nolikt) REFERENCES nolikt(id_nolikt))')
 		SQLinsert="""INSERT INTO tests(id_test,id_nolikt,testvalue) VALUES (%s,%s,%s)"""
 		SQLvalues=[(1,1,t1),(2,2,t3),(3,3,t2),(4,4,t4),(5,5,t2)]
-		self.cursor.executemany=(SQLinsert, SQLvalues)
+		self.cursor.executemany(SQLinsert, SQLvalues)
 		self.connection.commit()
-		# DESCRIBE is used to show information about the table, like the:
-		# Name of the column, column type and nullability (whether or not the value can be null or not)
-		# Key, PRI means primary key, UNI means Unique key, MUL means non-unique, MUL shows up if there's a foreign key
-		# Default value and then Extra as the last column (totalling 6 columns)
-		self.cursor.execute('DESCRIBE krasas')
-		for desc in self.cursor:
-			print(desc)
-		self.cursor.execute('DESCRIBE nolikt')
-		for desc in self.cursor:
-			print(desc)
-	# krassel and lietsel functions used to select and put them into a data list for the output
 	def krassel(self):
 		self.cursor.execute('SELECT * FROM krasas')
 		rez=[]
@@ -72,27 +64,31 @@ class Datubaze:
 		return rez2
 @app.route('/')
 def index():
-	#conn is used to be used a variable to connect with the database if not connected, afterwhich, data and data1 gathers information from krassel and lietsel respectively
+	#conn is used to be used a variable to connect with the database if not connected
 	global conn
 	if not conn:
 		conn=Datubaze()
 		conn.tabuluievade()
+	#data gathers information from the class Datubaze and then returns that data to index.html
 	data=conn.krassel()
 	data1=conn.lietsel()
 	data2=conn.testsel()
-	print("tu te")
-	return render_template('index.html',data=data, data1=data1, data2=data2)
+	return render_template('index.html', data=data, data1=data1, data2=data2)
 rezins='ip-lookup.html'
 @app.route('/ip-lookup', methods=["GET", "POST"])
 def ipinsert():
+	#create an error variable for error messages
 	error = None
 	info={}
 	if request.method == "POST":
+		#AiPe is a variable that gathers information from the user input, if the input is empty, an error message will appear
 		IP=request.form['AiPe']
 		if not IP:
 			error = "Empty Field, please input a valid IPv4 address"
 		else:
-			info=manafunkc.lookup(IP)
+		#sends IP data to backend python file, depending on the result, it will be returned to ip-lookup.html
+			info=functions.lookup(IP)
 	return render_template(rezins, error=error, look=info)
+#This starts the development server, checking whether the module is being run as the main program
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
---
+++ web/Flask-Mysql-Test/functions.py
@@ -0,0 +1,55 @@
+from flask import Flask
+import re
+import subprocess as su
+#creates a pattern for whitespace
+patterna=r"\s+"
+def check(IPs):
+	#this function checks if the data is a valid IPv4 address
+	#splits each number in to their own element, then checks if it's less than 4 elements, after which it checks if it is a digit, then the range between 0 and 255
+	l=IPs.split(".")
+	if len(l)!=4:
+		return False
+	for x in l:
+		if not x.isdigit():
+			return False
+		i=int(x)
+		if i < 0 or i > 255:
+			return False
+	return True
+def lookup(IP):
+	#in this loop cycle, it removes all commas if exists and whitespaces
+	IP=[I.strip() for I in IP.split(",")]
+	d={}
+	#the first if statement checks each IP address, if there exists IP addresses with a whitespace, then Error message will appear
+	for IPs in IP:
+		vai_ir=re.findall(patterna, IPs)
+		if any(ir.isspace() for ir in vai_ir):
+			d[IPs]={'ISPI':"Space has been detected between two or more IP address', please put a comma", 'valst': "No information"}
+		else:
+			#gathers information from check function if the IP address is a valid IPv4 address
+			checked=check(IPs)
+			if checked==False:
+				d[IPs]={'ISPI':"Not a valid IP address", 'valst': "No information"}
+			else:
+				#getwhois gathers ASN information from IPv4 address, whoispopen stores it in a "file" which then decodes it and reads the information
+				#output variable replaces all the whoispopen variable with empty
+				getwhois=['whois','-h','whois.cymru.com', IPs]
+				whoispopen=su.Popen(getwhois, stdout=su.PIPE)
+				whoispopen=whoispopen.stdout.read().decode('utf-8')
+				output=whoispopen.replace("\n","")
+				#Index then gathers the last 4 elements from the output, if "| NA exists in index variable, then No information has been gathered"
+				index=output[-4:]
+				if not output:
+					d[IPs]={'ISPI':"No information", 'valst': "No information"}
+				elif "| NA" in index:
+					d[IPs]={'ISPI':"No information", 'valst': "No information"}
+				else:
+				#removes unnecessary stuff, for cleaner output, splitting the "| " into each element, but getting the information from the 3rd element
+                                #ISP splits the comma of AS Name (*company name*, *country*) and gathers information from the first element, in this case company name
+				#valsts gathers the last 2 characters from output for the country name
+					output=output.replace("AS      | IP               | AS Name","")
+					spliteris=output.split("| ")[2]
+					ISP=spliteris.split(", ")[0]
+					valsts=output[-2:]
+					d[IPs]={'ISPI':ISP, 'country': valsts}
+	return d
