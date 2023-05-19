d85f34808c2d2c6714c01b31813d6a11b5fc0947
Ekluzivs <marks.zvagul@gmail.com>
2023-05-19 17:56:08 +0300

    vairākas mysql tabulas tiek izveidotas vienlaikus

--- web/Flask-Mysql-Test/controller.py
+++ web/Flask-Mysql-Test/controller.py
@@ -4,11 +4,6 @@ import mysql.connector
 #Assign the templates folder
 app=Flask(__name__, template_folder='templates')
 conn=None
-# Assnigning INT to variables for inserting into table value
-t1=200
-t2=21525
-t3=0
-t4=2
 #Further note, the issue of why the database was not working, because the database wasn't created, apparently the database has to created before launching the docker-compose command
 #to create database: docker exec -it (container-id) bash
 #mysql -u root -p (after which you enter the password that is on the python file)
@@ -19,26 +14,28 @@ class Database:
 	#This function is used to connect with the database if called by one of the other functions
 	def __init__(self, user='root', password='Parole1', database='mydb', host='db'):
 		self.connection=mysql.connector.connect(user=user, password=password, database=database, host=host)
+		self.connection.autocommit=True
 		self.cursor=self.connection.cursor()
 	#this function drops, creates and fills out the tables
-	def create_table(self):
+	def create_table(self, table_name,s1,s2,s3,s4):
+		drop=f'DROP TABLE IF EXISTS {table_name}'
+		self.cursor.execute(drop)
+		print("hi")
+		create_t=f"CREATE TABLE {table_name}({s1} INT AUTO_INCREMENT PRIMARY KEY NOT NULL, {s2} TEXT not null, {s3} TEXT, {s4} TEXT)"
+		self.cursor.execute(create_t)
+
 #The table that has foreign keys has to be dropped first, in this case tests, becuse nolikt and krasas are foreign keys, then nolikt because krasas is a foreign key, then krasas
-		self.cursor.execute('DROP TABLE IF EXISTS IP_mysql')
-
-		self.cursor.execute('CREATE TABLE IP_mysql(id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, IP_key TEXT not null, ISP TEXT, valsts TEXT)')
 		#Creates a test table with multiple foreign keys connecting it, this table tests multiple foreign keys and python variables
-		self.connection.commit()
-
 	def check_IP(self):
 		sql_delete='DELETE t1 FROM IP_mysql t1 INNER JOIN IP_mysql t2 WHERE t1.id<t2.id AND t1.IP_key = t2.IP_key AND t1.ISP=t2.ISP AND t1.valsts=t2.valsts'
 		self.cursor.execute(sql_delete)
-		self.connection.commit()
-	def insert_IP(self, IPs, ISP, country):
+	def drop_table(self):
+		self.cursor.execute('DELETE FROM IP_mysql')
+	def insert_IP(self, IPs, ISP, country, table_name):
 		sql_insert='INSERT INTO IP_mysql(IP_key, ISP, valsts) VALUES(%s,%s,%s)'
 		sql_values=[(IPs, ISP, country)]
 		self.cursor.executemany(sql_insert, sql_values)
-		self.connection.commit()
-	def IP_select(self):
+	def IP_select(self, table_name):
 		self.cursor.execute('SELECT * FROM IP_mysql')
 		select=self.cursor.fetchall()
 		return select
@@ -53,18 +50,35 @@ def ip_insert():
 	global conn
 	data={}
 	data_out={}
+	list=[
+		["IP_mysql", "id","IP_key","ISP","valsts"],
+		["domain_db","id","domens","IP","VT_link"]
+	]
+	if not conn:
+		conn=Database()
+		for lst in list:
+			table_name=lst[0]
+			s1=lst[1]
+			s2=lst[2]
+			s3=lst[3]
+			s4=lst[4]
+			print(table_name, s1,s2,s3,s4)
+			conn.create_table(table_name, s1,s2,s3,s4)
 	if request.method == "POST":
 		#AiPe is a variable that gathers information from the user input, if the input is empty, an error message will appear
 		IP=request.form['AiPe']
 		if not IP:
 			error = "Tukšs lauks, lūdzu ievadiet derīgu IPv4 adresi"
+		elif request.form.get('clear_btn') is not None:
+			if conn:
+				conn.drop_table()
+			data_out.clear()
+			data.clear()
+			return render_template(rezins)
 		else:
 		#sends IP data to backend python file, depending on the result, it will be returned to ip-lookup.html
 			info=functions.lookup(IP)
 			print(info)
-			if not conn:
-				conn=Database()
-				conn.create_table()
 			for ip in info:
 				data=info[ip]
 				ISP=data['ISPI']
@@ -72,6 +86,7 @@ def ip_insert():
 				conn.insert_IP(ip, ISP, country)
 			conn.check_IP()
 			data_out=conn.IP_select()
+			print(data_out)
 	return render_template(rezins, error=error, look=data_out)
 #This starts the development server, checking whether the module is being run as the main program
 if __name__ == '__main__':
